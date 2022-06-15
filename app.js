62fa513c9a6c52773ff70a8702f40111fd51e608
faikarghas <ghassanfaikar@yahoo.com>
2022-06-16 01:07:21 +0700

    setup utils

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,Intents.FLAGS.GUILD_MESSAGE_REACTIONS,Intents.FLAGS.GUILD_MEMBERS ],
     partials: ['MESSAGE','CHANNEL','REACTION']
 });
-const {TOKEN,welcomeChannelId,guildId} = require('./config.js')
+const {TOKEN,welcomeChannelId,guildId} = require('./utils/config.js')
 const generateImage = require('./generateImage')
 const levels = require('./commands/levels.js')
 
